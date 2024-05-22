9109a436317425fa5a20dcc29ec452e3180b6c58
Vikash kumar <Vraj67207@gmail.com>
2024-05-22 10:34:37 +0530

    observer

--- app/src/main/java/com/example/observer/MainActivity.kt
+++ app/src/main/java/com/example/observer/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.observer
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Lifecycle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycle.addObserver(Observer())
+        Log.d(TAG, "Activity main  ")
 
     }
 }
